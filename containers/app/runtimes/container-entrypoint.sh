cd4786a4581a454a4180121b900e69f15bbf1b59
francoism90 <francoism90@users.noreply.github.com>
2025-05-12 19:46:43 +0200

    Update container-entrypoint.sh: change SPOTWEB_USER validation from 'docker' to 'www-data'

--- containers/app/runtimes/container-entrypoint.sh
+++ containers/app/runtimes/container-entrypoint.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-if [ "$SPOTWEB_USER" != "root" ] && [ "$SPOTWEB_USER" != "docker" ]; then
-    echo "You should set SPOTWEB_USER to either 'docker' or 'root'."
+if [ "$SPOTWEB_USER" != "root" ] && [ "$SPOTWEB_USER" != "www-data" ]; then
+    echo "You should set SPOTWEB_USER to either 'www-data' or 'root'."
     exit 1
 fi
 
 if [ ! -z "$UID" ]; then
-    usermod -u $UID docker
+    usermod -u $UID www-data
 fi
 
 if [ "$1" != "" ]; then
